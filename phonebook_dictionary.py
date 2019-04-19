76a70ae894a47330b211aaf13cb9d4aa6d0b7f69
RJ Salamanca <salamancaronito@gmail.com>
2019-04-19 09:00:18 -0400

    added phonebook entries

---
+++ phonebook_dictionary.py
@@ -0,0 +1,20 @@
+phonebook_dict = {
+    'Alice' : '703-493-1834',
+    'Bob' : '857-384-1234',
+    'Elizabeth' : '484-584-2923'
+}
+
+# 1. Print Elizabeth's phone number
+print(phonebook_dict['Elizabeth'])
+
+# 2. Add an entry to the dictionary: Kareem's number is 938-489-1234.
+phonebook_dict['Kareem'] = '938-489-1234'
+
+# 3. Delete Alice's [phone entry.
+phonebook_dict.pop('Alice')
+
+# 4. Change Bob's phone number to '968-345-2345'.
+phonebook_dict['Bob'] = '968-345-2345'
+
+# 5. Print all the phone entries.
+print(phonebook_dict)
